1908425683eedd878b459daefb36ee5fe8383a63
gauravchauhan <chauhangaurav74@gmail.com>
2020-03-30 14:54:08 +0200

    Height/Maximum Depth and Minimum Depth Of binary tree using recursion.

---
+++ src/test/java/trees/binary/problemSolving/HeightOrMaximumDepthUsingRecursion.java
@@ -0,0 +1,44 @@
+package trees.binary.problemSolving;
+
+import trees.binary.BinaryTreeNode;
+
+public class HeightOrMaximumDepthUsingRecursion {
+
+    public Integer getHeightUsingRecursion(BinaryTreeNode node) {
+        if (null == node) return 0;
+
+        //using math function :
+        return Math.max(getHeightUsingRecursion(node.getLeft()), getHeightUsingRecursion(node.getRight())) + 1;
+
+//        //Using normal comparison operator:
+//        Integer leftSubtreeHeight = getHeightUsingRecursion(node.getLeft());
+//        Integer rightSubtreeHeight = getHeightUsingRecursion(node.getRight());
+//
+//        return leftSubtreeHeight > rightSubtreeHeight ? leftSubtreeHeight + 1 : rightSubtreeHeight + 1;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        BinaryTreeNode rootLeft = new BinaryTreeNode(2);
+        BinaryTreeNode rootRight = new BinaryTreeNode(3);
+        BinaryTreeNode rootLeftLeft = new BinaryTreeNode(4, null, null);
+        BinaryTreeNode rootLeftRight = new BinaryTreeNode(5, null, null);
+//        BinaryTreeNode rootRightLeft = new BinaryTreeNode(6, null, null);
+//        BinaryTreeNode rootRightRight = new BinaryTreeNode(7, null, null);
+
+        root.setLeft(rootLeft);
+        root.setRight(rootRight);
+
+        rootLeft.setLeft(rootLeftLeft);
+        rootLeft.setRight(rootLeftRight);
+
+//        rootRight.setLeft(rootRightLeft);
+//        rootRight.setRight(rootRightRight);
+
+        rootLeftLeft.setLeft(new BinaryTreeNode(8, new BinaryTreeNode(9), null));
+
+        Integer result = new HeightOrMaximumDepthUsingRecursion().getHeightUsingRecursion(root);
+
+        System.out.println(result);
+    }
+}
